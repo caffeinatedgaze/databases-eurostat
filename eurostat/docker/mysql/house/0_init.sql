ffee0ffbf3ee74b86266a549b0ef48d4f8fc75b4
Mikhail Liamets <m.liamets@jacobs-university.de>
2023-05-02 22:15:37 +0200

    Prettify

--- eurostat/docker/mysql/house/0_init.sql
+++ eurostat/docker/mysql/house/0_init.sql
@@ -1,30 +1,27 @@
-create table if not exists eurostat.Aggregated_Location
-(
-    id          int          not null
-        primary key,
-    description varchar(255) null
-);
+CREATE TABLE IF NOT EXISTS eurostat.aggregated_location
+  (
+     id          INT NOT NULL PRIMARY KEY,
+     description VARCHAR(255) NULL
+  );
 
-create table if not exists eurostat.Real_Location
-(
-    id   int          not null
-        primary key,
-    name varchar(255) null
-);
+CREATE TABLE IF NOT EXISTS eurostat.real_location
+  (
+     id   INT NOT NULL PRIMARY KEY,
+     name VARCHAR(255) NULL
+  );
 
-create table if not exists eurostat.House_Price
-(
-    id                     int auto_increment
-        primary key,
-    price                  float      null,
-    quarter                int        null,
-    location_id_real       int        null,
-    location_id_aggregated int        null,
-    year                   int        null,
-    is_real                tinyint(1) null,
-    constraint House_Price_ibfk_1
-        foreign key (location_id_real) references eurostat.Real_Location (id),
-    constraint House_Price_ibfk_2
-        foreign key (location_id_aggregated) references eurostat.Aggregated_Location (id),
-    check ((`location_id_real` is null) <> (`location_id_aggregated` is null))
-);
+CREATE TABLE IF NOT EXISTS eurostat.house_price
+  (
+     id                     INT auto_increment PRIMARY KEY,
+     price                  FLOAT NULL,
+     quarter                INT NULL,
+     location_id_real       INT NULL,
+     location_id_aggregated INT NULL,
+     year                   INT NULL,
+     is_real                TINYINT(1) NULL,
+     CONSTRAINT house_price_ibfk_1 FOREIGN KEY (location_id_real) REFERENCES
+     eurostat.real_location (id),
+     CONSTRAINT house_price_ibfk_2 FOREIGN KEY (location_id_aggregated)
+     REFERENCES eurostat.aggregated_location (id),
+     CHECK ((`location_id_real` IS NULL) <> (`location_id_aggregated` IS NULL))
+  ); 
