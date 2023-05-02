255041f99fb33b58bc04b4ecf9fc6887309d3399
Sergey Bogdanik <Sergey.Bogdanik@acronis.com>
2023-05-02 23:30:41 +0200

    Add house price queries

--- eurostat/app/queries/query_1.sql
+++ eurostat/app/queries/query_1.sql
@@ -1,1 +1,5 @@
-SELECT * FROM table_name WHERE ORDER BY id DESC LIMIT (%s); 
+SELECT Avg(price)                                         AS average_price,
+       Coalesce(location_id_real, location_id_aggregated) AS location_id
+FROM   house_price
+WHERE  year BETWEEN %s AND %s
+GROUP  BY location_id
