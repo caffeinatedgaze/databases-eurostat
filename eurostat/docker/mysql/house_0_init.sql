44250d30cce48261444f643aa18f811accd0fb54
Mikhail Liamets <m.liamets@jacobs-university.de>
2023-05-03 02:17:58 +0200

    Reupload housing

--- eurostat/docker/mysql/house_0_init.sql
+++ eurostat/docker/mysql/house_0_init.sql
@@ -1,16 +1,16 @@
-CREATE TABLE IF NOT EXISTS eurostat.aggregated_location
+CREATE TABLE IF NOT EXISTS aggregated_location
   (
      id          INT NOT NULL PRIMARY KEY,
      description VARCHAR(255) NULL
   );
 
-CREATE TABLE IF NOT EXISTS eurostat.real_location
+CREATE TABLE IF NOT EXISTS real_location
   (
      id   INT NOT NULL PRIMARY KEY,
      name VARCHAR(255) NULL
   );
 
-CREATE TABLE IF NOT EXISTS eurostat.house_price
+CREATE TABLE IF NOT EXISTS house_price
   (
      id                     INT auto_increment PRIMARY KEY,
      price                  FLOAT NULL,
@@ -20,8 +20,8 @@ CREATE TABLE IF NOT EXISTS eurostat.house_price
      year                   INT NULL,
      is_real                TINYINT(1) NULL,
      CONSTRAINT house_price_ibfk_1 FOREIGN KEY (location_id_real) REFERENCES
-     eurostat.real_location (id),
+     real_location (id),
      CONSTRAINT house_price_ibfk_2 FOREIGN KEY (location_id_aggregated)
-     REFERENCES eurostat.aggregated_location (id),
+     REFERENCES aggregated_location (id),
      CHECK ((`location_id_real` IS NULL) <> (`location_id_aggregated` IS NULL))
   ); 
