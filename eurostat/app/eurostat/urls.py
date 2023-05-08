0f875e429d614c1f2abb1dbf02572423ec2205c6
Mikhail Liamets <m.liamets@jacobs-university.de>
2023-05-08 20:08:34 +0200

    Add search endpoint

--- eurostat/app/eurostat/urls.py
+++ eurostat/app/eurostat/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import main_view, query_view
+from .views import main_view, query_view, search
 
 urlpatterns = [
     path("", main_view),
     path("query/<int:num>", query_view),
+    path("search/", search),
 ]
