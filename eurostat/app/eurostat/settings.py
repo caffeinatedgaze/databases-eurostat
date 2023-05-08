daba912c2f40322ac4d019c5330c9400be9f7c6a
Mikhail Liamets <m.liamets@jacobs-university.de>
2023-05-08 21:13:12 +0200

    Change default flag

--- eurostat/app/eurostat/settings.py
+++ eurostat/app/eurostat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "mysecretkey"
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
